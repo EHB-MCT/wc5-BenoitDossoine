9d74f16b8fcd382237b0414217d55a251cd3b489
Benoit Dossoine <benoit.dossoine@student.ehb.be>
2021-03-18 13:53:56 +0100

    The login form works, the token gets stored in the local storage, the messages are rendered and it is possible to send a message.

--- js/index.js
+++ js/index.js
@@ -1,14 +1,41 @@
 "use strict";
 
 const messageSystem = {
-  startFetching() {
-  },
+  startFetching() {},
 
   sendMessage(msg) {
+    const message = {
+      message: msg
+    };
+    fetch("https://thecrew.cc/api/message/create.php?token=" + userSystem.token, {
+        method: 'POST',
+        body: JSON.stringify(message)
+      })
+      .then(response => response.json)
+      .then(data => {
+        this.fetchMessages();
+      });
     // https://thecrew.cc/api/message/create.php?token=__TOKEN__ POST
   },
 
   fetchMessages() {
+    fetch("https://thecrew.cc/api/message/read.php?token=" + userSystem.token, {
+        method: 'GET',
+      })
+      .then(response => response.json())
+      .then(data => {
+        const messageWall = document.getElementById("output");
+        messageWall.innerHTML = '';
+        data.forEach(function (value, index) {
+          const message = document.createElement("div");
+          message.className = "message";
+          message.innerHTML = `<span class="by">${value.handle} </span>
+          <span class="on">${value.created_at} </span>
+          <p>${value.message}</p>`;
+          messageWall.appendChild(message);
+        });
+
+      });
     // https://thecrew.cc/api/message/read.php?token=__TOKEN__ GET
   }
 };
@@ -25,11 +52,39 @@ const userSystem = {
     return localStorage.getItem("token");
   },
 
-  logout() { 
+  checkLogin() {
+    const token = this.getToken();
+    if (token != null) {
+      this.token = token;
+      display.removeLogin();
+      messageSystem.fetchMessages();
+    }
+
+  },
+
+  logout() {
     localStorage.removeItem("token");
   },
 
   login(email, password) {
+
+    const user = {
+      "email": email,
+      "password": password
+    };
+
+    fetch("https://thecrew.cc/api/user/login.php", {
+        method: 'POST',
+        body: JSON.stringify(user)
+      })
+      .then(response => response.json())
+      .then(data => {
+        this.token = data.token;
+        this.saveToken();
+        display.removeLogin();
+        messageSystem.fetchMessages();
+      });
+
     // https://thecrew.cc/api/user/login.php POST
   },
 
@@ -40,7 +95,30 @@ const userSystem = {
 
 const display = {
   initFields() {
+    const formLogin = document.getElementById("loginForm");
+    formLogin.addEventListener("submit", this.loginHandler);
+    const formMessage = document.getElementById("messageForm");
+    formMessage.addEventListener("submit", this.messageHandler);
+  },
+
+  loginHandler(e) {
+    e.preventDefault();
+    const email = document.getElementById("emailField").value;
+    const password = document.getElementById("passwordField").value;
+    userSystem.login(email, password);
+  },
+
+  messageHandler(e) {
+    e.preventDefault();
+    const message = document.getElementById("messageField").value;
+    messageSystem.sendMessage(message);
+  },
+
+  removeLogin() {
+    document.getElementById("loginWindow").style.display = "none";
   }
 };
 
-  display.initFields();
+
+display.initFields();
+userSystem.checkLogin();
